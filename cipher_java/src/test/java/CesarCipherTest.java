7aac889ccc6df80b90fe060e94677161d532ec01
Diego Gomez Canonico <diegogcanonico@gmail.com>
2022-10-10 06:11:11 -0300

    Se incorporan unit tests

--- cipher_java/src/test/java/CesarCipherTest.java
+++ cipher_java/src/test/java/CesarCipherTest.java
@@ -1,2 +1,186 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import roo2.CesarCipher;
+
+
 public class CesarCipherTest {
+    private CesarCipher cesarCipher;
+    private CesarCipher cesarCipherWith2Parameters;
+
+
+
+    @BeforeEach
+    void setUp(){
+        cesarCipher = new CesarCipher(3);
+        cesarCipherWith2Parameters = new CesarCipher(3, "abcdefghijklmnopqrstuvwxyz");
+    }
+
+
+    @Test
+    public void cipherTestOk(){
+        //Arrange
+        String parametro = "Hola";
+        String esperado = "Cesar Cipher:Hrod";
+        //Act
+        String resultado = cesarCipher.cipher(parametro);
+        //Assert
+        Assert.assertEquals(esperado, "Cesar Cipher:"+resultado);
+    }
+
+    @Test
+    public void cipherTestOkWithNumericParameter(){
+        //Arrange
+        String parametro = "123";
+        String esperado = "Cesar Cipher:123";
+        //Act
+        String resultado = cesarCipher.cipher(parametro);
+        //Assert
+        Assert.assertEquals(esperado, "Cesar Cipher:"+resultado);
+    }
+
+    @Test
+    public void cipherTestOkWithSymbolParameter(){
+        //Arrange
+        String parametro = "$%&";
+        String esperado = "Cesar Cipher:$%&";
+        //Act
+        String resultado = cesarCipher.cipher(parametro);
+        //Assert
+        Assert.assertEquals(esperado, "Cesar Cipher:"+resultado);
+    }
+
+    @Test
+    public void cipherTestNotOkWithNullParameter(){
+        //Arrange
+        String parametro = null;
+        //Act
+        //Assert
+        Assert.assertThrows(NullPointerException.class,
+                ()->{cesarCipher.cipher(parametro);} );
+    }
+
+    @Test
+    public void cipherTestNotOkWithEmptyArray(){
+        //Arrange
+        String[] parametro = new String[1];
+        //Act
+        //Assert
+        Assert.assertThrows(NullPointerException.class,
+                ()->{cesarCipher.cipher(parametro[0]);} );
+    }
+
+    @Test
+    public void decipherTestOk(){
+        //Arrange
+        String parametro = "Hrod";
+        String esperado = "Cesar Cipher:Hola";
+        //Act
+        String resultado = cesarCipher.decipher(parametro);
+        //Assert
+        Assert.assertEquals(esperado, "Cesar Cipher:"+resultado);
+    }
+
+    @Test
+    public void decipherTestOkWithNumericParameter(){
+        //Arrange
+        String parametro = "123";
+        String esperado = "Cesar Cipher:123";
+        //Act
+        String resultado = cesarCipher.decipher(parametro);
+        //Assert
+        Assert.assertEquals(esperado, "Cesar Cipher:"+resultado);
+    }
+
+    @Test
+    public void decipherTestOkWithSymbolParameter(){
+        //Arrange
+        String parametro = "$%&";
+        String esperado = "Cesar Cipher:$%&";
+        //Act
+        String resultado = cesarCipher.decipher(parametro);
+        //Assert
+        Assert.assertEquals(esperado, "Cesar Cipher:"+resultado);
+    }
+
+    @Test
+    public void decipherTestNotOkWithNullParameter(){
+        //Arrange
+        String parametro = null;
+        //Act
+        //Assert
+        Assert.assertThrows(NullPointerException.class,
+                ()->{cesarCipher.decipher(parametro);} );
+    }
+
+    @Test
+    public void decipherTestNotOkWithEmptyArray(){
+        //Arrange
+        String[] parametro = new String[1];
+        //Act
+        //Assert
+        Assert.assertThrows(NullPointerException.class,
+                ()->{cesarCipher.decipher(parametro[0]);} );
+    }
+
+
+    @Test
+    public void cipherTestWith2ParametersOk(){
+        //Arrange
+        String parametro = "Hola";
+        String esperado = "Cesar Cipher:Hrod";
+        //Act
+        String resultado = cesarCipherWith2Parameters.cipher(parametro);
+        //Assert
+        Assert.assertEquals(esperado, "Cesar Cipher:"+resultado);
+    }
+
+    @Test
+    public void cipherTestWith2Parameters2Ok(){
+        //Arrange
+        CesarCipher cesarCipherWith2Parameters = new CesarCipher(3,"");
+        String parametro = "Hola";
+        String esperado = "Cesar Cipher:Hola";
+        //Act
+        String resultado = cesarCipherWith2Parameters.cipher(parametro);
+        //Assert
+        Assert.assertEquals(esperado, "Cesar Cipher:"+resultado);
+    }
+
+    @Test
+    public void cipherTestWith2NumericParametersOk(){
+        //Arrange
+        CesarCipher cesarCipherWith2Parameters = new CesarCipher(3,"123456789");
+        String parametro = "Hola";
+        String esperado = "Cesar Cipher:Hola";
+        //Act
+        String resultado = cesarCipherWith2Parameters.cipher(parametro);
+        //Assert
+        Assert.assertEquals(esperado, "Cesar Cipher:"+resultado);
+    }
+
+
+    @Test
+    public void cipherTestWith2ParametersWith10Ok(){
+        //Arrange
+        CesarCipher cesarCipherWith2Parameters = new CesarCipher(10,"abcdefghijklmnopqrstuvwxyz");
+        String parametro = "Hola";
+        String esperado = "Cesar Cipher:Hyvk";
+        //Act
+        String resultado = cesarCipherWith2Parameters.cipher(parametro);
+        //Assert
+        Assert.assertEquals(esperado, "Cesar Cipher:"+resultado);
+    }
+
+    @Test
+    public void cipherTestWith2ParametersNotOk(){
+        //Arrange
+        CesarCipher cesarCipherWith2Parameters = new CesarCipher(-10,"abcdefghijklmnopqrstuvwxyz");
+        String parametro = "Hola";
+        //Act
+        //Assert
+        Assert.assertThrows(ArrayIndexOutOfBoundsException.class,
+                ()->{cesarCipherWith2Parameters.cipher(parametro);} );
+    }
+
 }
