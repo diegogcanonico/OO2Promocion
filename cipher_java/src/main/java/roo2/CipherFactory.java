eb12f03d60f51872dddd6245cd0e0b60d6abd3bd
Diego Gomez Canonico <diegogcanonico@gmail.com>
2022-11-13 06:25:37 -0300

    Se borra cesar y se adapta vigenere para que genere un cesar

--- cipher_java/src/main/java/roo2/CipherFactory.java
+++ cipher_java/src/main/java/roo2/CipherFactory.java
@@ -2,9 +2,19 @@ package roo2;
 
 public class CipherFactory {
 
-
     public Cipher getCesarCipher(String alfabeto, int salto){
-        return new CesarCipher(alfabeto,salto);
+        int offset = salto;
+        int longitudAlfabeto = alfabeto.length();
+        if (offset >= longitudAlfabeto) {
+            while (offset >= longitudAlfabeto) {
+                offset = offset - longitudAlfabeto;
+            }
+        }
+        String clave = String.valueOf(alfabeto.charAt(offset));
+        for (int j = 0; j <= 25; j++) {
+            clave += alfabeto.charAt(offset);
+        }
+        return new VigenereCipher(alfabeto, clave);
     }
 
     public Cipher getVigenereCipher(String alfabeto, String clave){
